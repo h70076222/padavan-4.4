e17e87baaa5c6c8cd25c0f60eba62a27eb74a4c9
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-08-28 17:39:41 +0800

    Update vpn.sh

--- trunk/user/hxsdwan/vpn.sh
+++ trunk/user/hxsdwan/vpn.sh
@@ -41,14 +41,14 @@ sleep 20 && /usr/bin/vpn.sh &
 :<<'________'
 VPN异地组网配置区
 串码 yhtfhgdf #组网串码 所有同一组网必须同一名 如：abcd
-#以下改IP参数，虚似IP最后一位也要对应改，和-d要一起改
-#如改本地192.168.30.1,-d 就改30 -o改10.26.0.30
-#远端改(另一个路由)-i 192.168.30.0/24,10.26.0.30
+#以下改IP参数，虚似IP最后一位也要对应改，和路由的名要一起改
+#如改本地192.168.30.1,路由的名就改30 本机虚拟IP改10.26.0.30
+#远端改(另一个路由)192.168.30.0/24,10.26.0.30
 #服务器
 路由的名 22     #路由的名字，不能和组网同名 如：-d  1 2 3
-对网路由IP 192.168.12.0/24,10.26.0.12   # 对端路由IP,对端路由的虚拟IP 例如:-i 192.168.1.0/24,10.26.0.11
-本机IP 192.168.33.0/24 #   本路由IP 如：-o 192.168.1.0/24
-本机虚拟IP 10.26.0.33 #   本路由的虚拟IP 如：--ip 10.26.0.11
+对网路由IP 192.168.12.0/24,10.26.0.12   # 对端路由IP,对端路由的虚拟IP 例如:192.168.1.0/24,10.26.0.11
+本机IP 192.168.33.0/24 #   本路由IP 如:192.168.1.0/24
+本机虚拟IP 10.26.0.33 #   本路由的虚拟IP 如:10.26.0.11
 
 ________
 EOF10
@@ -65,15 +65,15 @@ fi
  [ -n "`cat $boot | grep -o '\本机虚拟IP'`" ] && finger=$(cat $boot | grep '\本机虚拟IP' | awk -F '\本机虚拟IP' '{print $2}'|awk -F '#' '{print $1}' )
 
  
-echo "./vpn $aswr $white $white_token $gateway $netmask &"
+echo "./vpn -k $port $white -d $white_token -i $gateway -o $netmask --ip $finger &"
 
 vpn_dirname=$(dirname ${vpn})
 
-cd $vpn_dirname && ./vpn $aswr $white $white_token $gateway $netmask &
+cd $vpn_dirname && ./vpn -k $port $white -d $white_token -i $gateway -o $netmask --ip $finger  &
 
 sleep 3
 if [ ! -z "`pidof vpn`" ] ; then
-logger -t "vpn" "启动成功"
+logger -t "异地组网" "启动成功"
 #放行vnt防火墙
 iptables -I INPUT -i vnt-tun -j ACCEPT
 iptables -I FORWARD -i vnt-tun -o vnt-tun -j ACCEPT
@@ -82,5 +82,5 @@ iptables -t nat -I POSTROUTING -o vnt-tun -j MASQUERADE
 #开启arp
 ifconfig vnt-tun arp
 else
-logger -t "vpn" "启动失败"
+logger -t "异地组网" "启动失败"
 fi
