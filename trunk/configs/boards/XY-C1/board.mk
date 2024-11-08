4dc7aa538102d4a4f3b4f4419e01cb029538b95f
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-11-09 00:11:14 +0800

    Update board.mk

--- trunk/configs/boards/XY-C1/board.mk
+++ trunk/configs/boards/XY-C1/board.mk
@@ -4,6 +4,6 @@
 # XY-C1   # XY-C1          # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_XY_C1
+CFLAGS += -DBOARD_K2P
 BOARD_NUM_USB_PORTS=2
 CONFIG_BOARD_RAM_SIZE=128
